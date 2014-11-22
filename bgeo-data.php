c63ffe6e1a47e38ee9d6bdabebf8e095e4023459
Casey Bisson <casey.bisson@gmail.com>
2014-11-22 14:43:53 -0800

    trying to fix a bug with lock handling

--- bgeo-data.php
+++ bgeo-data.php
@@ -499,7 +499,10 @@ class bGeo_Data extends WP_CLI_Command
 	private function get_lock( $woeid )
 	{
 		$i = 0;
-		while ( $lock = wp_cache_get( $woeid, 'bgeo-data-lock', TRUE ) )
+		while (
+			$lock = wp_cache_get( $woeid, 'bgeo-data-lock', TRUE ) &&
+			(bool) $lock
+		)
 		{
 			if ( 1000 < $i )
 			{
